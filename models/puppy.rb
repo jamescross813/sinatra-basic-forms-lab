10ebeadd954385bc175d2241ab128c84fdf941ab
jamescross <crossjames813@gmail.com>
2021-04-19 17:04:25 -0500

    commit tree 0e0efc95615cf512af454fc51cecc166e692fb1a

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,16 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+class Puppy
+
+attr_reader :breed
+attr_accessor :age, :name
+
+def initialize(params)
+    @name = params[:name]
+    @age = params[:age]
+    @breed = params[:breed]
+# binding.pry
+end
+
+
+
+end
